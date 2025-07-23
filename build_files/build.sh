2f6b74294f3656cd36674ebb0ae43b9d588f9d7d
Kohega <45211407+Kohega@users.noreply.github.com>
2025-07-23 17:26:16 +0200

    Add Kodi, Syncthing and Zerotier

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,8 @@
 
 set -ouex pipefail
 
+### Create root directory for hdd mount points 
+mkdir /data /games
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +12,24 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y kodi kodi-inputstream-adaptive syncthing
+
+## Install ZeroTierOne
+# Setup repo
+cat << EOF > /etc/yum.repos.d/zerotier.repo
+[zerotier]
+name=ZeroTier, Inc. RPM Release Repository
+baseurl=http://download.zerotier.com/redhat/fc/22
+enabled=1
+gpgcheck=0
+EOF
+
+# Install Zerotier
+dnf install -y zerotier-one
+
+# Remove Zerotier repo
+rm /etc/yum.repos.d/zerotier.repo -f
+
 
 # Use a COPR Example:
 #
@@ -19,6 +38,7 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+systemctl enable podman.socket syncthing@kohega.service
